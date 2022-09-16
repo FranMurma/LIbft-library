fe30080bc4a57dcce7010e9b68533624c9ce99a7
FranMurma <111235202+FranMurma@users.noreply.github.com>
2022-09-16 19:58:36 +0200

    Create ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	cont;
+
+	cont = 0;
+	while (s[cont] != '\0')
+	{
+		if (s[cont] == (char)c)
+			return ((char *)(s + cont));
+		cont++;
+	}
+	if (s[cont] == (char)c)
+		return ((char *)(s + cont));
+	return (NULL);
+}
