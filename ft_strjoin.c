31e3f38776beda99dd9f5693c42c402c36c75886
FranMurma <111235202+FranMurma@users.noreply.github.com>
2022-09-28 12:19:02 +0200

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,35 +1,25 @@
+#include "libft.h
+
 char  *ft_strjoin(char const *s1, char const *s2)
 {
   int len_s1;
   int len_s2;
-  int len_s3;
   
-  len_s1 = 0;
-  while (s1[len_s1])
-    len_1++;
-  len_s2 = 0;
-  while (s2[len_s2])
-    len_2++;
-  len_s3 = len_s1 + len_s2;
-  s3 = malloc (sizeof (char) * len_s3 +1);
+  len_s1 = ft_strlen(s1);
+  len_s2 = ft_strlen(s2);
+  s3 = malloc (sizeof (char) * (len_s1 + len_s2 +1));
   if (!s3)
     return (NULL);
-  len_s1 = 0;
-  len_s3 = 0;
-  while (s1[len_s1])
-  {
-    s3[len_s3] = s1[len_s1];
-    len_s3++;
-    len_s1++;
-  }
   while (s2[len_s2])
   {
-    s3[len_s3] = s2[len_s2]
-    {
-      len_s3++;
-      len_s2++;
-    }
-    len_s3 = '\0';
-    return (s3);
+    s3[len_s1 + len_s2] = s2[len_s2];
+    len_s2--;
   }
+  while (s1[len_s1])
+  {
+    s3[len_s1 + len_s2] = s1[len_s1]
+      len_s1--;
+  }
+  s3[len_s1 + len_s2 + 1] = '\0';
+  return (s3);
 }
