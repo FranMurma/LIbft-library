8ec6b5157e9bce284190f4661ff6490265bc0e6b
FranMurma <111235202+FranMurma@users.noreply.github.com>
2022-09-29 20:17:01 +0200

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: frmurcia <frmurcia@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 17:07:06 by frmurcia          #+#    #+#             */
-/*   Updated: 2022/09/29 19:15:44 by frmurcia         ###   ########.fr       */
+/*   Updated: 2022/09/29 20:16:41 by frmurcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ void	ft_free(char **str_n)
 		free (str_n[cont]);
 		cont++;
 	}
+	free (str_n);
 }
 
-size_t  ft_words(char const *s, char c)
+int	ft_words(char const *s)
 {
     int cont_s;
     int words;
@@ -32,12 +33,16 @@ size_t  ft_words(char const *s, char c)
     cont_s = 0;
     words = 0;
 	while (s[cont_s])
-    if ((s[cont_s + 1] == c || s[cont_s + 1] = '\0') && s[cont_s] != c)
-        words++;
+	{
+		if ((s[cont_s + 1] == c || s[cont_s + 1] = '\0') && s[cont_s] != c)
+			words++;
+		cont_s++;
+	}
+	return (words);
 }
 
 char	**ft_split(char const *s, char c)
-{
+{i
 	int	cont_s;
 	int	cont_n;
 	int	words;
@@ -46,14 +51,22 @@ char	**ft_split(char const *s, char c)
 	cont_s = 0;
 	cont_n = 0;
 	start = 0;
-	words = ft_words(s, c);
+	words = ft_words(s);
 	s_n = (char **) malloc(sizeof (char*) * (words + 1));
     if (!s_n[cont_n])
-		ft_free(s_n);
-	if ((s[cont_s] == c && s[cont_s + 1] != c) || (s[cont_s + 1] = '\0'))
-		start = cont_s + 1;
-	cont_s++;
-	s_n[cont_n] = ft_substr(s, start, cont_s - start - 1);
-	cont_n++;
-
-
+		return (NULL);
+	while (s_n[cont_s])
+	{
+		if ((s[cont_s] == c && s[cont_s + 1] != c) || (s[cont_s + 1] = '\0'))
+		{
+			s_n[cont_n] = ft_substr(s, start, cont_s - start + 1);
+			if (!s_n[cont])
+			{
+				ft_free(s_n);
+				return (NULL);
+			}
+		}
+	
+		cont_s++;
+	}
+		
