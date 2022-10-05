35fcd1c768373981249e24db58d7f66ebd1520b4
FranMurma <111235202+FranMurma@users.noreply.github.com>
2022-10-05 11:23:27 +0200

    Create ft_lstnew.c

---
+++ ft_lstnew.c
@@ -0,0 +1,11 @@
+t_list *ft_lstnew(void *content)
+{
+  t_list  nodo;
+  
+  nodo = (t_list *)malloc(sizeof(struct s_list));
+  if (!nodo)
+    return (NULL);
+  nodo -> content = content;
+  nodo -> next = NULL;
+  return (nodo);
+}
